44b7d81fc7e5f40dc244662826bb0f756bb44fd9
Sushant More <moresushant48@gmail.com>
2021-07-15 12:11:01 +0530

    Setup Browse Button & fill path entry.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from progList import openNewWindow
-from tkinter import Frame, font, ttk
-from tkinter.constants import BOTH, BOTTOM, HORIZONTAL, LEFT, TOP, X, Y
+from tkinter import Frame, font, ttk, filedialog
+from tkinter.constants import BOTH, BOTTOM, HORIZONTAL, LEFT, RIGHT, TOP, X, Y
 import pyttsx3 as tts
 import os
 import tkinter as tk
@@ -30,9 +30,13 @@ root = style.master
 root.title("PlanItOut")
 root.geometry("500x350")
 
-main = Frame(root)
-main.pack(
-    side=TOP, fill=BOTH, padx=16, pady=16)
+inputFrame = Frame(root)
+inputFrame.pack(
+    side=TOP, fill=BOTH, padx=16)
+
+buttonFrame = Frame(root)
+buttonFrame.pack(
+    side=TOP, fill=BOTH, padx=16)
 
 varName = tk.StringVar(root)
 varPath = tk.StringVar(root)
@@ -45,27 +49,36 @@ def updatePaths():
         lstBox.insert(tk.END, '{} : {}'.format(item, paths[item]))
 
 
+def file_opener():
+    input = filedialog.askopenfilename(
+        filetypes=[("Applications", "*.exe")],)
+    varPath.set(input)
+
+
 def openPaths():
     for item in paths:
         tts.speak("Opening " + item)
         os.startfile(paths[item])
 
 
-ttk.Label(main, text='Enter Application Name',
+ttk.Label(inputFrame, text='Enter Application Name',
           style='primary.TLabel').pack(pady=yPad, fill=X)
-ttk.Entry(main, textvariable=varName).pack(pady=yPad, fill=X)
+ttk.Entry(inputFrame, textvariable=varName).pack(pady=yPad, fill=X)
 
-ttk.Label(main, text='Enter Application Path',
+ttk.Label(inputFrame, text='Enter Application Path',
           style='primary.TLabel').pack(pady=yPad, fill=X)
-ttk.Entry(main, textvariable=varPath).pack(pady=yPad, fill=X)
+ttk.Entry(inputFrame, textvariable=varPath).pack(pady=yPad, fill=X)
 
-ttk.Separator(main, orient=HORIZONTAL,
-              style='primary.Horizontal.TSeparator').pack(pady=yPad, fill=X)
+ttk.Button(inputFrame, text='Browse',
+           command=file_opener).pack(side=LEFT, pady=yPad, fill=X, expand=1)
 
-ttk.Button(main, text='Add',
+# ttk.Separator(buttonFrame, orient=HORIZONTAL,
+#               style='primary.Horizontal.TSeparator').pack(pady=yPad, fill=X)
+
+ttk.Button(buttonFrame, text='Add',
            command=updatePaths).pack(side=LEFT, padx=1, pady=yPad, fill=X, expand=1)
 
-ttk.Button(main, text='Open',
+ttk.Button(buttonFrame, text='Open',
            command=openPaths).pack(side=LEFT, padx=1, pady=yPad, fill=X, expand=1)
 
 ttk.Button(root, text='Show All Programs', style='primary.Outline.TButton',
