1aa41a207a8547b3e11c0ffa77b83a50bc294ade
Sushant More <moresushant48@gmail.com>
2021-07-23 12:18:28 +0530

    build.bat : Build with consoles for Testing purposes.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
-pyinstaller -n PlanItOut --onefile main.py --add-data Symbola.ttf;ttkbootstrap --add-data themes.json;ttkbootstrap --clean --uac-admin --noconsole
-pyinstaller -n runner --onefile .\runner.py --noconsole
+pyinstaller -n PlanItOut --onefile main.py --add-data Symbola.ttf;ttkbootstrap --add-data themes.json;ttkbootstrap --clean --uac-admin  
+:: --noconsole
+pyinstaller -n runner --onefile .\runner.py
+:: --noconsole
