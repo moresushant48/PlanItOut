05f761eda8d9a01f35c7232a977d45f96119bdbb
Sushant More <moresushant48@gmail.com>
2021-07-23 12:16:11 +0530

    build.bat to generate EXEs.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

---
+++ build.bat
@@ -0,0 +1,2 @@
+pyinstaller -n PlanItOut --onefile main.py --add-data Symbola.ttf;ttkbootstrap --add-data themes.json;ttkbootstrap --clean --uac-admin --noconsole
+pyinstaller -n runner --onefile .\runner.py --noconsole
